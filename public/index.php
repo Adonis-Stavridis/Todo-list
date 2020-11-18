d497ca5def83473585f8fbdc8b0a6299b7cd5ad1
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-11-18 18:08:06 +0100

    index cleaning

--- public/index.php
+++ public/index.php
@@ -2,24 +2,10 @@
 declare(strict_types=1);
 session_start();
 
-use Slim\Factory\AppFactory;
+use Todo\TodoApp;
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../app/env.php';
 
-$dependencies = require __DIR__ . '/../app/dependencies.php';
-$container = $dependencies();
-
-$settings = require __DIR__ . '/../app/settings.php';
-$settings($container);
-
-AppFactory::setContainer($container);
-
-$app = AppFactory::create();
-
-$middleware = require __DIR__ . '/../app/middleware.php';
-$middleware($app);
-
-require __DIR__ . '/../app/routes.php';
+$app = new TodoApp();
 
 $app->run();
---
+++ src/TodoApp.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Todo;
+
+use Slim\App;
+use Slim\Factory\AppFactory;
+
+class TodoApp {
+  private App $app;
+
+  public function __construct() {
+    require __DIR__ . '/../app/env.php';
+
+    $dependencies = require __DIR__ . '/../app/dependencies.php';
+    $container = $dependencies();
+
+    $settings = require __DIR__ . '/../app/settings.php';
+    $settings($container);
+
+    AppFactory::setContainer($container);
+
+    $app = AppFactory::create();
+
+    $middleware = require __DIR__ . '/../app/middleware.php';
+    $middleware($app);
+
+    require __DIR__ . '/../app/routes.php';
+
+    $this->app = $app;
+  }
+
+  public function run() {
+    $this->app->run();
+  }
+}
